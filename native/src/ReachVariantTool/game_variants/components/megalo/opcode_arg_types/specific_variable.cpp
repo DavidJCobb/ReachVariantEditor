475ae4b3e6b87e33482c127d60e35c59fe99cfbd
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2020-02-10 02:29:44 +0100

    OpcodeArgTypeinfo progress
    Added typeinfos to a few more types.
    
    To make that easier on me, OpcodeArgValueStringTokens2 is no longer templated, as Halo: Reach doesn't use any other token counts; it's always two tokens. IIRC Halo 4 does have varying token counts but supporting Halo 4 isn't a design goal of the "REACH Variant Tool."

---
+++ native/src/ReachVariantTool/game_variants/components/megalo/opcode_arg_types/specific_variable.cpp
@@ -0,0 +1,85 @@
+#include "specific_variable.h"
+
+namespace Megalo {
+   bool OpcodeArgValueContextualVariableBaseClass::read(cobb::ibitreader& stream) noexcept {
+      bool absence = stream.read_bits(1) != 0;
+      if (absence)
+         return true;
+      auto scope = getScopeObjectForConstant(this->baseScope);
+      this->index = stream.read_bits(scope.index_bits(this->baseType));
+      return true;
+   }
+   void OpcodeArgValueContextualVariableBaseClass::write(cobb::bitwriter& stream) const noexcept {
+      if (this->index == -1) {
+         stream.write(1, 1);
+         return;
+      }
+      stream.write(0, 1);
+      auto scope = getScopeObjectForConstant(this->baseScope);
+      stream.write(this->index, scope.index_bits(this->baseType));
+   }
+   void OpcodeArgValueContextualVariableBaseClass::to_string(std::string& out) const noexcept {
+      const char* owner = "INVALID";
+      switch (this->baseScope) {
+         case variable_scope::object: owner = "object"; break;
+         case variable_scope::player: owner = "player"; break;
+         case variable_scope::team:   owner = "team";   break;
+      }
+      const char* type  = "INVALID";
+      switch (this->baseType) {
+         case variable_type::object: type = "Object"; break;
+         case variable_type::player: type = "Player"; break;
+         case variable_type::scalar: type = "Number"; break;
+         case variable_type::team:   type = "Team";   break;
+         case variable_type::timer:  type = "Timer";  break;
+      }
+      if (this->index == -1) {
+         cobb::sprintf(out, "the %s in question's %s[None] variable", owner, type);
+         return;
+      }
+      cobb::sprintf(out, "the %s in question's %s[%u] variable", owner, type, this->index);
+   }
+   //
+   //
+   //
+   OpcodeArgTypeinfo OpcodeArgValueObjectTimerVariable::typeinfo = OpcodeArgTypeinfo(
+      OpcodeArgTypeinfo::typeinfo_type::default,
+      0,
+      &OpcodeArgValueObjectTimerVariable::factory
+   );
+   //
+   //
+   //
+   OpcodeArgTypeinfo OpcodeArgValueObjectPlayerVariable::typeinfo = OpcodeArgTypeinfo(
+      OpcodeArgTypeinfo::typeinfo_type::default,
+      0,
+      &OpcodeArgValueObjectPlayerVariable::factory
+   );
+   //
+   bool OpcodeArgValueObjectPlayerVariable::read(cobb::ibitreader& stream) noexcept {
+      if (!this->object.read(stream))
+         return false;
+      bool absence = stream.read_bits(1) != 0;
+      if (absence)
+         return true;
+      this->playerIndex = stream.read_bits(MegaloVariableScopeObject.index_bits(variable_type::player));
+      return true;
+   }
+   void OpcodeArgValueObjectPlayerVariable::write(cobb::bitwriter& stream) const noexcept {
+      this->object.write(stream);
+      if (this->playerIndex == -1) {
+         stream.write(1, 1);
+         return;
+      }
+      stream.write(0, 1);
+      stream.write(this->playerIndex, MegaloVariableScopeObject.index_bits(variable_type::player));
+   }
+   void OpcodeArgValueObjectPlayerVariable::to_string(std::string& out) const noexcept {
+      this->object.to_string(out);
+      if (this->playerIndex == -1) {
+         out += " - no/all? players";
+         return;
+      }
+      cobb::sprintf(out, "%s.Player[%d]", out.c_str(), this->playerIndex);
+   }
+}
---
+++ native/src/ReachVariantTool/game_variants/components/megalo/opcode_arg_types/tokens.cpp
@@ -0,0 +1,105 @@
+#include "tokens.h"
+#include "../../../types/multiplayer.h"
+#include "object.h"
+#include "player.h"
+#include "scalar.h"
+#include "team.h"
+#include "timer.h"
+
+namespace Megalo {
+   bool OpcodeStringToken::read(cobb::ibitreader& stream) noexcept {
+      this->type.read(stream);
+      switch (this->type) {
+         case OpcodeStringTokenType::none:
+            break;
+         case OpcodeStringTokenType::player:
+            this->value = new OpcodeArgValuePlayer();
+            break;
+         case OpcodeStringTokenType::team:
+            this->value = new OpcodeArgValueTeam();
+            break;
+         case OpcodeStringTokenType::object:
+            this->value = new OpcodeArgValueObject();
+            break;
+         case OpcodeStringTokenType::number:
+         //case OpcodeStringTokenType::number_with_sign:
+            this->value = new OpcodeArgValueScalar();
+            break;
+         case OpcodeStringTokenType::timer:
+            this->value = new OpcodeArgValueTimer();
+            break;
+         default:
+            return false;
+      }
+      if (this->value) {
+         this->value->read(stream);
+      }
+      return true;
+   }
+   void OpcodeStringToken::write(cobb::bitwriter& stream) const noexcept {
+      this->type.write(stream);
+      if (this->value)
+         this->value->write(stream);
+   }
+   void OpcodeStringToken::to_string(std::string& out) const noexcept {
+      if (this->value)
+         this->value->to_string(out);
+   }
+   //
+   //
+   //
+   OpcodeArgTypeinfo OpcodeArgValueStringTokens2::typeinfo = OpcodeArgTypeinfo(
+      OpcodeArgTypeinfo::typeinfo_type::default,
+      0,
+      &OpcodeArgValueStringTokens2::factory
+   );
+   //
+   bool OpcodeArgValueStringTokens2::read(cobb::ibitreader& stream) noexcept {
+      this->stringIndex.read(stream); // string table index pointer; -1 == none
+      this->tokenCount.read(stream);
+      if (this->tokenCount > max_token_count) {
+         printf("Tokens value claimed to have %d tokens; max is %d.\n", this->tokenCount.to_int(), max_token_count);
+         return false;
+      }
+      for (uint8_t i = 0; i < this->tokenCount; i++)
+         this->tokens[i].read(stream);
+      return true;
+   }
+   void OpcodeArgValueStringTokens2::postprocess(GameVariantDataMultiplayer* newlyLoaded) noexcept {
+      if (this->stringIndex >= 0) {
+         auto& list = newlyLoaded->scriptData.strings;
+         if (this->stringIndex < list.size())
+            this->string = list.strings[this->stringIndex];
+      }
+      for (uint8_t i = 0; i < this->tokenCount; i++)
+         this->tokens[i].value->postprocess(newlyLoaded);
+   }
+   void OpcodeArgValueStringTokens2::write(cobb::bitwriter& stream) const noexcept {
+      this->stringIndex.write(stream);
+      this->tokenCount.write(stream);
+      for (uint8_t i = 0; i < this->tokenCount; i++)
+         this->tokens[i].write(stream);
+   }
+   void OpcodeArgValueStringTokens2::to_string(std::string& out) const noexcept {
+      if (this->tokenCount == 0) {
+         if (this->stringIndex == -1) {
+            out = "no string";
+            return;
+         }
+         cobb::sprintf(out, "localized string ID %d", this->stringIndex);
+         return;
+      }
+      out.clear();
+      for (uint8_t i = 0; i < this->tokenCount; i++) {
+         std::string temp;
+         //
+         auto& token = this->tokens[i];
+         token.to_string(temp);
+         //
+         if (!out.empty())
+            out += ", ";
+         out += temp;
+      }
+      cobb::sprintf(out, "format string ID %d and tokens: %s", this->stringIndex, out.c_str());
+   }
+}
