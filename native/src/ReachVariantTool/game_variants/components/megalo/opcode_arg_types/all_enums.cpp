51806f3198b9d717e63ade0293ee60df06976ea5
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2020-05-18 15:40:40 +0200

    Halo: Reach doesn't support bitshift operators

--- native/src/ReachVariantTool/game_variants/components/megalo/opcode_arg_types/all_enums.cpp
+++ native/src/ReachVariantTool/game_variants/components/megalo/opcode_arg_types/all_enums.cpp
@@ -51,10 +51,11 @@ namespace Megalo {
          DetailedEnumValue("&=", DetailedEnumValueInfo::make_friendly_name("bitwise-AND with")),
          DetailedEnumValue("|=", DetailedEnumValueInfo::make_friendly_name("bitwise-OR with")),
          DetailedEnumValue("^=", DetailedEnumValueInfo::make_friendly_name("bitwise-XOR with")),
-         DetailedEnumValue("~=", DetailedEnumValueInfo::make_friendly_name("bitwise-NOT with")),
-         DetailedEnumValue("<<=",  DetailedEnumValueInfo::make_friendly_name("bitshift left by")),
-         DetailedEnumValue(">>=",  DetailedEnumValueInfo::make_friendly_name("bitshift right by")),
-         DetailedEnumValue("<<<=", DetailedEnumValueInfo::make_friendly_name("arith? shift left by")),
+         DetailedEnumValue("~=", DetailedEnumValueInfo::make_friendly_name("bitwise-NOT with")), // (a ~= b) == (a &= ~b)
+         //
+         DetailedEnumValue("+=",  DetailedEnumValueInfo::make_friendly_name("add(?)")), // KSoft calls this <<= but it acts like addition in MCC-Reach tests
+         //DetailedEnumValue("",  DetailedEnumValueInfo::make_friendly_name("crash the game with")), // KSoft calls this >>= but it crashes in tests
+         //DetailedEnumValue("<<<=", DetailedEnumValueInfo::make_friendly_name("untested-operation with")),
       });
       auto pickup_priority = DetailedEnum({
          DetailedEnumValue("normal"),
